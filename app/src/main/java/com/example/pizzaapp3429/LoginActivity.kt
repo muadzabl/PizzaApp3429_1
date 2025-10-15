d47343a04eab3bd4481026d805a00f17ca8452fb
muadzabl <ablmuadz78@students.amikom.ac.id>
2025-10-15 10:39:10 +0700

    initial commit

---
+++ app/src/main/java/com/example/pizzaapp3429/LoginActivity.kt
@@ -0,0 +1,29 @@
+package com.example.pizzaapp3429
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val btnLogin: Button = findViewById(R.id.buttonLogin)
+        //event -> di klik
+        btnLogin.setOnClickListener {
+            //inten -> memanggil activity login
+            val intenLogin = Intent(this, AccountActivity::class.java)
+            startActivity(intenLogin)
+        }
+    }
+}
